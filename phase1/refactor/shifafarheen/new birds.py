1528f3e7ab69c9cda5ebf6dbef41377706e15d12
Shifa Farheen <shifafarheen637@gmail.com>
2026-01-30 23:33:17 -0800

    Add Bird and Ostrich classes with fly methods

---
+++ phase1/refactor/shifafarheen/new birds.py
@@ -0,0 +1,13 @@
+class Bird:
+    def fly(self):
+        print("flying")
+        class Ostrich("bird"):
+            def fly(self):
+                print("ostrich cannot fly")
+
+                bird=Bird()
+                ostrich=Ostrich()
+                
+            
+                bird.fly()
+                ostrich.fly()
--- phase1/src/birds.py
+++
@@ -1,7 +0,0 @@
-class Bird:
-    def fly(self):
-        print("Flying")
-
-class Ostrich(Bird):
-    def fly(self):
-        raise Exception("Ostrich can't fly")
