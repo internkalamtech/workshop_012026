1f5101442f27f8a201d0968b855702f8100e5256
sheeba <sheeba@gmail.com>
2026-01-31 13:08:38 +0530

    machine

--- phase1/refactor/shabeer/birds.py
+++ phase1/refactor/shabeer/birds.py
@@ -1,1 +1,17 @@
 #Good code for birds module.
+class bird:
+    def fly(self):
+        print("Bird is flying")
+
+class ostrich(bird):
+    def fly(self):
+         raise Exception("ostrich can't fly")
+if __name__ == "__main__":
+     bird = bird()
+     bird.fly()
+
+     ostrich = ostrich()
+     try:
+         ostrich.fly()
+     except Exception as e:
+         print(e)
---
+++ phase1/refactor/shabeer/refactor/noorain/machine.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+class Printer(ABC):
+    @abstractmethod
+    def print(self):
+        pass
+class Scanner(ABC):
+    @abstractmethod
+    def scan(self):
+        pass
+class Fax(ABC):
+    @abstractmethod
+    def fax(self):
+        pass
+
+class OldPrinter(Printer):
+    def print(self):
+        print("Printing")
+class ModernPrinter(Printer,Scanner,Fax):
+    def print(self):
+        print("Modern printing")
+
+    def scan(self):
+        print("scanning")
+
+    def fax(self):
+        print("faxing")
+# ...existing code...
+
+if __name__ == "__main__":
+    old = OldPrinter()
+    old.print()
+
+    modern = ModernPrinter()
+    modern.print()
+    modern.scan()
+    modern.fax()
