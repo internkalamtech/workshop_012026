0aa54bb6cac9c190e72d36ca13a7c844a6264d00
zyx <xyz@gmail.com>
2026-01-31 12:49:43 +0530

    fixed issues

--- phase1/refactor/shabeer/birds.py
+++ phase1/refactor/shabeer/birds.py
@@ -1,1 +1,14 @@
-#Good code for birds module.
+class bird:
+    def fly(self):
+        print("bird is flying")
+class Ostrich(bird):
+    def fly(self):
+        raise Exception("Ostrich can't fly")    
+if __name__ == "__main__":
+    bird = bird()
+    bird.fly()
+    ostrich = Ostrich()
+    try:
+         ostrich.fly()   
+    except Exception as e:
+            print(e)
