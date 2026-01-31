41793911f5d33dff3e255808b0e7195b62855991
far <far@gmail.com>
2026-01-31 12:51:35 +0530

    birds

--- phase1/refactor/shabeer/birds.py
+++ phase1/refactor/shabeer/birds.py
@@ -1,1 +1,16 @@
 #Good code for birds module.
+class Bird:
+    def fly(self):
+        print("Flying")
+class Ostrich(Bird):
+    def fly(self):
+        raise Exception("Ostrich can't fly")        
+if __name__ == "__main__":
+    bird = Bird()
+    bird.fly()  # Output: Flying
+
+    ostrich = Ostrich()
+    try:
+        ostrich.fly()
+    except Exception as e:
+        print(e)  # Output: Ostrich can't fly
