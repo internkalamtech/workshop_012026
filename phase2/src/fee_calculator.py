2e667d60698a0f13dbbb53fdd0eca5ce6d77df47
Mohammed Ayan Ahmed <ayanahmed12006@gmail.com>
2026-01-31 15:14:54 +0530

    message

--- phase2/src/fee_calculator.py
+++ phase2/src/fee_calculator.py
@@ -9,7 +9,7 @@
 
 def calculate_fee(student_type, base_fee):
     if student_type == "science":
-        return base_fee + 500
+        return base_fee + 600
     elif student_type == "commerce":
         return base_fee + 300
     else:
