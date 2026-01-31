67986133c1406bd4f158c05f5a960d7c45d90902
breeze993 <khairunnisa2005ar@gmail.com>
2026-01-31 13:07:32 +0530

    Add Database and  classes;  moduleFeeService

---
+++ phase1/refactor/Khairunnisa/machine1.py
@@ -0,0 +1,11 @@
+
+        class Database:
+    def save(self, name, amount):
+        print(f"Saving {name} - {amount}")
+
+class FeeService:
+    def pay_fee(self, student_name, amount):
+        db = Database()
+        db.save(student_name, amount)
+        fee = FeeService()
+        fee.pay_fee("Alice", 1000)
